e5e1dcf3508b79a66aad11ac0d7cda07a3d88096
Yash Jagtap <jagtapyash2512@gmail.com>
2024-06-20 15:31:50 +0530

    update mapping for PostCreated

--- src/lens-proxy.ts
+++ src/lens-proxy.ts
@@ -1,46 +1,20 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  Upgraded as UpgradedEvent
+  PostCreated as PostCreatedEvent
 } from "../generated/LensProxy/LensProxy"
-import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
+import { PostCreated } from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+export function handlePostCreated(event: PostCreatedEvent): void {
+  let entity = new PostCreated(
+    Bytes.fromUTF8(
+      event.params.profileId.toString() + "-" + event.params.pubId.toString(),
+    ),
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.contentURI = event.params.contentURI;
 
-  entity.save()
-}
+  entity.ownerId = event.params.profileId;
+  entity.timestamp = event.params.timestamp;
 
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
